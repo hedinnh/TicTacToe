654caddb28b9c48791d9c3b22276753b3e804cf0
hedinnh <hedinn08@ru.is>
2017-10-24 16:54:56 +0000

    gradle build script and test class added

--- src/main/java/is/ru/ticTacToe/TicTacToe.java
+++ src/main/java/is/ru/ticTacToe/TicTacToe.java
@@ -1,9 +1,10 @@
-package ticTacToe.TicTacToe.src.main.java.is.ru.ticTacToe;
+//package ticTacToe.TicTacToe.src.main.java.is.ru.ticTacToe;
 
 public class TicTacToe {
 
 	public static void main(String[] args) {
-
+		System.out.println("test");
+		
 
 	}
 
