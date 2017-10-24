39a400096b24cb1e30779c639f16b0fbac4176ef
Oliprik <oliprik121@gmail.com>
2017-10-24 20:04:38 +0000

    Added createboard function

--- src/main/java/is/ru/ticTacToe/TicTacToe.java
+++ src/main/java/is/ru/ticTacToe/TicTacToe.java
@@ -1,10 +1,23 @@
 package is.ru.ticTacToe;
 
 public class TicTacToe {
+	private char[] board;
 
+	public static char[][] createBoard()
+	{
+		char[][] newBoard = new char[3][3];
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+			newBoard[i][j] = '-';
+			}
+		}
+		return newBoard;
+	}
 	public static void main(String[] args) {
 		System.out.println("test");
-		
+
 
 	}
 
--- src/test/java/is/ru/ticTacToe/TicTacToeTest.java
+++ src/test/java/is/ru/ticTacToe/TicTacToeTest.java
@@ -1,6 +1,23 @@
+package is.ru.ticTacToe;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TicTacToeTest{
-    
+    @Test
+    public void testCreateBoard()
+    {
+        char[][] expected = new char[3][3];
+        expected[0][0] = '-';
+        expected[0][1] = '-';
+        expected[0][2] = '-';
+        expected[1][0] = '-';
+        expected[1][1] = '-';
+        expected[1][2] = '-';
+        expected[2][0] = '-';
+        expected[2][1] = '-';
+        expected[2][2] = '-';
+
+        assertEquals(expected, TicTacToe.createBoard());
+    }
 }
