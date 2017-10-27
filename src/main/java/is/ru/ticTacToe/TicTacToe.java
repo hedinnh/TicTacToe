8dbf4e741ab27545ccc7da259e6197299c66de6f
hedinnh <hedinn08@ru.is>
2017-10-27 23:48:45 +0000

    Added boardfull and printboard function. also fixed createboard function

--- src/main/java/is/ru/ticTacToe/TicTacToe.java
+++ src/main/java/is/ru/ticTacToe/TicTacToe.java
@@ -1,24 +1,69 @@
 package is.ru.ticTacToe;
 
-public class TicTacToe {
-	private char[] board;
 
+import edu.princeton.cs.algs4.*;
+
+public class TicTacToe {
+	private static char[][] board;
+	private static boolean done;
+	private static boolean playerTurn = false;
+	/**
+	 * Constructor, set variables and call createboard
+	 */
+	public TicTacToe() {
+		done = false;
+		createBoard();
+	}
+	/** Create a 3x3[][] board for TicTacToe game
+	 * @return [][] board
+	 */
 	public static char[][] createBoard()
 	{
-		char[][] newBoard = new char[3][3];
+		board = new char[3][3];
 		for(int i = 0; i < 3; i++)
 		{
 			for(int j = 0; j < 3; j++)
 			{
-			newBoard[i][j] = '-';
+				board[i][j] = '-';
 			}
 		}
-		return newBoard;
+		
+		return board;
 	}
+	/** 
+	 * Print the multidimensional array to console
+	 */
+	public void printBoard()
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
+
+	}
+	public void boardFull()
+	{
+		int count = 0;
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++)
+			{
+				if(board[i][j] != '-')
+					count++;
+			}
+		}
+		if(count == 9)
+			done = true;
+	}
+
+
+
 	public static void main(String[] args) {
-		System.out.println("test");
-
-
+		
 	}
-
 }
