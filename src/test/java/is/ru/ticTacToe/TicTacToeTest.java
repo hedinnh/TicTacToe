7d0bdd1a1198f96b937b077919d966e2246d3ae3
Garthar <gardar14@ru.is>
2017-10-29 18:54:35 +0000

    cleaned up 'testCreateBoard' test

--- src/test/java/is/ru/ticTacToe/TicTacToeTest.java
+++ src/test/java/is/ru/ticTacToe/TicTacToeTest.java
@@ -4,20 +4,15 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TicTacToeTest{
-    @Test
-    public void testCreateBoard()
-    {
+    @Test 
+    public void testCreateBoard() {
         char[][] expected = new char[3][3];
-        expected[0][0] = '-';
-        expected[0][1] = '-';
-        expected[0][2] = '-';
-        expected[1][0] = '-';
-        expected[1][1] = '-';
-        expected[1][2] = '-';
-        expected[2][0] = '-';
-        expected[2][1] = '-';
-        expected[2][2] = '-';
-
+        for(int i = 0; i <= 2; i++){
+            for(int j = 0; j <= 2; j++){
+                 expected[i][j] = '-';
+            }
+        }
         assertEquals(expected, TicTacToe.createBoard());
     }
+
 }
