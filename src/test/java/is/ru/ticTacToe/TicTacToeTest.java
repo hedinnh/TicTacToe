cad174303cf81e21334e987e5b733d125d57fb03
Garthar <gardar14@ru.is>
2017-10-29 19:56:10 +0000

    added tests for x and o victories (including cross section victory) and a test for a drawed game

--- src/test/java/is/ru/ticTacToe/TicTacToeTest.java
+++ src/test/java/is/ru/ticTacToe/TicTacToeTest.java
@@ -7,8 +7,7 @@ public class TicTacToeTest{
 
     TicTacToe ttt = new TicTacToe();
     @Test
-    public void testCreateBoard()
-    {
+    public void testCreateBoard(){
         char[][] expected = new char[3][3];
         for(int i = 0; i <= 2; i++){
             for(int j = 0; j <= 2; j++){
@@ -19,8 +18,7 @@ public class TicTacToeTest{
     }
 
     @Test
-    public void testMakeMove()
-    {
+    public void testMakeMove(){
         ttt.makeMove(1);
         assertEquals("X--------",ttt.getBoard());
         ttt.makeMove(5);
@@ -30,6 +28,48 @@ public class TicTacToeTest{
         ttt.makeMove(6);
         ttt.makeMove(8);
         assertEquals("XX--OO-X-", ttt.getBoard());
+    }
+
+    @Test
+    public void testWinO(){
+        ttt.makeMove(1);
+        ttt.makeMove(3);
+        ttt.makeMove(2);
+        ttt.makeMove(6);
+        ttt.makeMove(7);
+        ttt.makeMove(9);
+
+        assertEquals('O', ttt.checkWin());
+    }
+
+    @Test
+    public void testWinX(){
+        ttt.makeMove(1);
+        ttt.makeMove(2);
+        ttt.makeMove(3);
+        ttt.makeMove(4);
+        ttt.makeMove(7);
+        ttt.makeMove(8);
+        ttt.makeMove(9);
+        ttt.makeMove(6);
+        ttt.makeMove(5);
+
+        assertEquals('X', ttt.checkWin());
+    }
+
+    @Test
+    public void testDraw(){
+        ttt.makeMove(5);
+        ttt.makeMove(3);
+        ttt.makeMove(1);
+        ttt.makeMove(9);
+        ttt.makeMove(6);
+        ttt.makeMove(4);
+        ttt.makeMove(7);
+        ttt.makeMove(2);
+        ttt.makeMove(8);
+
+        assertEquals('E', ttt.checkWin());
     }
 
 }
